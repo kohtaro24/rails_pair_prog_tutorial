7c3dcf63f222448757fffbae02cfb252520d3933
kohtaro24 <koutaro.nishi.fb@gmail.com>
2021-07-17 03:45:53 +0900

    システム設定編集画面を実装

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,8 +4,19 @@ class ContractsController < ApplicationController
   end
 
   def edit
+    @contract = Contract.find(params[:id])
   end
 
   def update
+    @contract = Contract.find(params[:id])
+    @contract.update!(update_params)
+
+    redirect_to contract_path(params[:id])
+  end
+
+  private
+
+  def update_params
+    params.require(:contract).permit(:name, :users_quantity)
   end
 end
